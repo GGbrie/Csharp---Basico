8a13dc26fce5a702ffeaaddda3b81c80c5accc81
GGbrie <106721167+GGbrie@users.noreply.github.com>
2023-03-03 22:11:50 -0600

    Add files via upload

---
+++ Ejercicios Programación I/Ejercicios Programación I/Program.cs
@@ -0,0 +1,31 @@
+﻿// Condiciones
+
+using System;
+
+namespace Sentencias
+{
+    class Program5
+    {
+        public static void Main(string[] args)
+        {
+
+            int numeroEntero = int.Parse(Console.ReadLine());
+
+            if (numeroEntero == 7)
+            {
+                Console.WriteLine("Numero es 7.");
+            }
+            else
+            {
+                if (numeroEntero < 0)
+                {
+                    Console.WriteLine("numeroEntero es negativo");
+                }
+                else
+                {
+                    Console.WriteLine("numeroEntero no es ninguno de los casos programados");
+                }
+            }
+        }
+    }
+}
