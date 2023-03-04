6537a403d3d056d2b9fbc25b7961c2867ac91c14
GGbrie <106721167+GGbrie@users.noreply.github.com>
2023-03-03 21:58:42 -0600

    Add files via upload

---
+++ Ejercicios Programación I/Ejercicios Programación I/Programa1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Programa1
+{
+    class Programacsharp1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hazlo Facil, haz Click!");
+        }
+    }
+}
