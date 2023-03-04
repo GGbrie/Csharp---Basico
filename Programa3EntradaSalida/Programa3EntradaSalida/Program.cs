8a5e559a66e778e05e61bbbaece5798f436b51a7
GGbrie <106721167+GGbrie@users.noreply.github.com>
2023-03-03 22:12:26 -0600

    Add files via upload

---
+++ Programa3EntradaSalida/Programa3EntradaSalida/Program.cs
@@ -0,0 +1,19 @@
+﻿//Entrada / Salida
+
+using System;
+
+namespace Fundamentos3
+{
+    class EntradaSalida
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.Write("Escribe tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.Read();
+        }
+    }
+}
